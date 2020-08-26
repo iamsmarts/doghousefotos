d433b101fb4e3ade66302484f4f51fe2bf1d51e1
Rudy Quevedo <iamsmarts@DESKTOP-0AGC3O4.localdomain>
2020-08-26 14:12:34 -0700

    ig link

--- src/Views/Home/Home.view.jsx
+++ src/Views/Home/Home.view.jsx
@@ -6,6 +6,17 @@ const Home = () => {
   return (
     <div>
       <Hero />
+      <div className="row justify-content-md-center ig-wrap">
+        <div className="col-md-4 align-self-center ig-home">
+          <a
+            href="https://www.instagram.com/doghousefotos/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./images/ig-big.png" alt="" />
+          </a>
+        </div>
+      </div>
       <hr />
       <HomeCopy />
     </div>
